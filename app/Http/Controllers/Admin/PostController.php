56a209ecf7485f7d2ad4b301f8d47b5de742dab2
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2021-12-23 18:57:29 +0330

    Remove show methods from admin controllers.

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -70,17 +70,6 @@ class PostController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Post  $post
